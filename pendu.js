738060b98933895b255e20aa0e90c1bffc07a92b
Vincent De Gheyndt <vincent.degheyndt@gmail.com>
2019-11-08 16:35:39 +0100

    maj3

--- pendu.js
+++ pendu.js
@@ -1,47 +1,40 @@
 
     let motAtrouvrer = "bonjour"
-    motAtrouvrer = motAtrouvrer.toUpperCase()
+    motAtrouvrer = motAtrouvrer;
     let tab1 = motAtrouvrer.split('');
-    
-    
+    let tab2 = [];//tableau des lettres entrées 
+    for(let lettre of motAtrouvrer){
+        tab2.push('*'); 
+    }
+    let i = 0; //compteur de fautes 
 
-    let tab2 = ['','','','','','',''];
-    let i = 0;
-    
-    
-    
-    
-  
-    
-    
-    let guessLetter = () => {
+    const guessLetter = () => {
         let lettre = prompt("esseyez une lettre");
-        let ind=tab1.indexOf(lettre);
-        
-        console.log(ind);
-        if (tab2.includes(lettre)){
-            alert ("vous avez déjà entré cette lettre");
-            i=i+1
-
-
+        if(tab2.includes(lettre)){
+            alert ("vous avez déjà entré cette lettre et vous avez raté " +i+ " fois");
+            guessLetter();
         }
-        else  if (tab1.includes(lettre)){
+        else if(tab1.includes(lettre)){
        
-        
-             tab2.push(lettre,(4));
+            let ind = tab1.indexOf(lettre);
+              while (ind != -1){
+                tab2.splice(ind, 1 , lettre);
+                ind= tab1.indexOf(lettre, ind + 1); 
+              }
              console.log(tab2)
-             
-             
-             i=i+1
+             if (tab1.join('') === tab2.join('')) {
+                alert('félicitation vous avez trouvé le mot');
+            }
+             guessLetter();
         }
-        else {
+        else  {
             i=i+1;
-            alert =("mauvaise lettre");
-            
-        }
-        
+            alert("mauvaise lettre , vous avez raté " +i+ " fois");
+            guessLetter();
+        } 
     }
     guessLetter();
+    console.log(tab1);
    
   
 
